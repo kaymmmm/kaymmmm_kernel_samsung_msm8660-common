bbb7cdb9d0f9add760d41ef89f355a8bc53e842c
elbermu <angbermu@gmail.com>
2013-02-01 20:52:53 -0400

    Enable max frequency changes

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -148,7 +148,9 @@ static struct dbs_tuners {
 	.ignore_nice = 0,
 	.powersave_bias = 0,
 	.freq_boost_time = DEFAULT_FREQ_BOOST_TIME,
+#ifdef CONFIG_SEC_DVFS
 	.boostfreq = MAX_FREQ_LIMIT,
+#endif	
 };
 
 static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
